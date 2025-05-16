47d6edf2a2eb9cfa70194a42247c99500e0f415f
caguirre88 <caguirre@selhauling.com>
2025-05-16 19:30:53 -0400

    Add Fibonacci DP calculator with GUI

---
+++ FibonacciDP.java
@@ -0,0 +1,17 @@
+public class FibonacciDP {
+    public static long fib(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be non-negative");
+        }
+        if (n <= 1) {
+            return n;
+        }
+        long[] dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
